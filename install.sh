302069b9c651ae056ef8f505f03b0e5ffb6b3099
Zubuntu <zjrosen1@gmail.com>
2014-07-24 15:34:51 -0400

    prompt for install script

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
+clear
 echo "Well congrats on taking the big step into my .ViM config!"
 echo ""
 echo "If you want to keep your .vimrc then back that shit up!"
 echo ""
-echo "I'm about to override anything you have in thurr"
-echo ""
-echo "Cool?"
-
-ln -sf ~/.vim/vimrc ~/.vimrc
-cd ~/.vim
- git submodule update --init
-
+while true; do
+	read -p "Do you want to continue y or n? " yn
+	case $yn in
+		[Yy]* ) 
+			ln -sf ~/.vim/vimrc ~/.vimrc ;
+			cd ~/.vim ;
+			git submodule update --init ;
+			break;;
+		[Nn]* ) echo "Bye";
+			exit;;
+		* ) echo "Please answer yes or no.";
+			echo "";;
+	esac
+done
